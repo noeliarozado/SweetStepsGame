7b9a0c2be0ace6dc0144930bc05ac1e2a8ef77a9
Noelia Rozado <noelia.rozado@gmail.com>
2025-01-23 17:05:01 +0100

    New scenes

---
+++ Scenes/DoughScene.cs
@@ -0,0 +1,104 @@
+using Godot;
+using System;
+
+public partial class DoughScene : Node2D
+{
+	private bool isDragging = false;  
+	private Sprite2D rollingPin;    
+	private Sprite2D dough;        
+	private ProgressBar progressBar; 
+
+	private float previousYPosition; 
+	private bool movedUp = false;   
+	private bool movedDown = false; 
+
+	private float kneadingProgress = 0; 
+
+	public override void _Ready()
+	{
+		rollingPin = GetNode<Sprite2D>("RollingPin");
+		dough = GetNode<Sprite2D>("Dough");
+		progressBar = GetNode<ProgressBar>("RollingProgressBar");
+
+		progressBar.Value = kneadingProgress;
+
+		previousYPosition = rollingPin.GlobalPosition.Y;
+	}
+
+	public override void _Process(double delta)
+	{
+		// Move the rolling pin with the mouse while dragging
+		if (isDragging)
+		{
+			rollingPin.GlobalPosition = GetViewport().GetMousePosition();
+
+			// Check if the rolling pin has moved up or down
+			float currentYPosition = rollingPin.GlobalPosition.Y;
+
+			if (currentYPosition < previousYPosition - 10) // Moved up by at least 10 pixels
+			{
+				movedUp = true;
+			}
+			else if (currentYPosition > previousYPosition + 10) // Moved down by at least 10 pixels
+			{
+				movedDown = true;
+			}
+
+			previousYPosition = currentYPosition; 
+
+			if (movedUp && movedDown)
+			{
+				movedUp = false;
+				movedDown = false;
+
+				// Check if the rolling pin overlaps the dough
+				Rect2 rollingPinRect = new Rect2(
+					rollingPin.GlobalPosition - (rollingPin.Texture.GetSize() * rollingPin.Scale / 2),
+					rollingPin.Texture.GetSize() * rollingPin.Scale
+				);
+
+				Rect2 doughRect = new Rect2(
+					dough.GlobalPosition - (dough.Texture.GetSize() * dough.Scale / 2),
+					dough.Texture.GetSize() * dough.Scale
+				);
+
+				if (rollingPinRect.Intersects(doughRect))
+				{
+					kneadingProgress += (float)delta * 50; 
+					progressBar.Value = kneadingProgress;
+
+					if (kneadingProgress >= 100)
+					{
+						GD.Print("Dough is ready!");
+						GetTree().ChangeSceneToFile("res://Scenes/flat_dough_scene.tscn");
+					}
+				}
+			}
+		}
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		// Start dragging when the left mouse button is pressed
+		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed && mouseEvent.ButtonIndex == MouseButton.Left)
+		{
+			Vector2 mousePosition = GetViewport().GetMousePosition();
+
+			Rect2 rollingPinRect = new Rect2(
+				rollingPin.GlobalPosition - (rollingPin.Texture.GetSize() * rollingPin.Scale / 2),
+				rollingPin.Texture.GetSize() * rollingPin.Scale
+			);
+
+			if (rollingPinRect.HasPoint(mousePosition))
+			{
+				isDragging = true;
+			}
+		}
+
+		// Stop dragging when the left mouse button is released
+		if (@event is InputEventMouseButton mouseEventRelease && !mouseEventRelease.Pressed && mouseEventRelease.ButtonIndex == MouseButton.Left)
+		{
+			isDragging = false;
+		}
+	}
+}
---
+++ Scenes/MixingIngredients.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+
+public partial class MixingIngredients : Node2D
+{
+	private ProgressBar mixProgressBar;
+	private Label messageLabel;
+	private Button mixButton;
+
+	private float progressSpeed = 50f;
+	private bool isMixing = false;
+
+	public override void _Ready()
+	{
+		mixProgressBar = GetNode<ProgressBar>("MixProgressBar");
+		messageLabel = GetNode<Label>("MessageLabel");
+		mixButton = GetNode<Button>("MixButton");
+
+		mixButton.Pressed += OnMixButtonPressed;
+	}
+
+	private void OnMixButtonPressed()
+	{
+		if (!isMixing)
+		{
+			isMixing = true;
+			mixButton.Disabled = true;
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		if (isMixing)
+		{
+			// Increase the progress bar value
+			mixProgressBar.Value += progressSpeed * (float)delta;
+
+			// Check if mixing is complete
+			if (mixProgressBar.Value >= mixProgressBar.MaxValue)
+			{
+				isMixing = false;
+				mixProgressBar.Value = mixProgressBar.MaxValue;
+
+				messageLabel.Text = "Ingredients mixed!";
+
+				// Wait for 1 second and change to the new scene
+				Timer timer = new Timer();
+				AddChild(timer);
+				timer.WaitTime = 1.0f;
+				timer.OneShot = true;
+				timer.Timeout += () =>
+				{
+					GetTree().ChangeSceneToFile("res://Scenes/dough_scene.tscn");
+				};
+				timer.Start();
+			}
+		}
+	}
+}
