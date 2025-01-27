8b13caba775ff141f1ed1e0a769778b1ca91dbc8
Noelia Rozado <noelia.rozado@gmail.com>
2025-01-27 17:20:44 +0100

    Decoration

--- Scenes/DecorationScene.cs
+++ Scenes/DecorationScene.cs
@@ -6,12 +6,18 @@ public partial class DecorationScene : Node2D
 	private Sprite2D starTray;   
 	private Button decoratingButton; 
 	private Button eatCookiesButton; 
+	private Label label; 
+	private Sprite2D sugar; 
+	private Sprite2D tableBackground;
 
 	public override void _Ready()
 	{
 		starTray = GetNode<Sprite2D>("StarTray");
 		decoratingButton = GetNode<Button>("DecoratingButton");
 		eatCookiesButton = GetNode<Button>("EatCookiesButton");
+		label = GetNode<Label>("Label");
+		sugar = GetNode<Sprite2D>("Sugar");
+		tableBackground = GetNode<Sprite2D>("TableBackground");
 
 		eatCookiesButton.Visible = false;
 
@@ -22,17 +28,20 @@ public partial class DecorationScene : Node2D
 	private void OnDecoratingButtonPressed()
 	{
 		starTray.Texture = GD.Load<Texture2D>("res://Images/icedcookies.png");
-		
 		starTray.Scale = new Vector2(0.75f, 0.75f);
 		starTray.Position = new Vector2(400, 270);
 		
 		decoratingButton.Visible = false;
+		label.Visible = false; 
+		sugar.Visible = false; 
+		
+		tableBackground.Texture = GD.Load<Texture2D>("res://Images/Cloth.jpg");
+		
 		eatCookiesButton.Visible = true;
 	}
 
 	private void OnEatCookiesButtonPressed()
 	{
-		GD.Print("Eating cookies... Goodbye!");
 		GetTree().Quit();
 	}
 }
